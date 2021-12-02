a80c8f2f3f7bb64b924e583cf12e17f9382fde0d
Steven <stntsai93@gmail.com>
2021-12-02 16:47:36 -0500

    add test message statement

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,27 +3,28 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode} = require('../compile');
 
+let accounts;
+let inbox;
 
-class Car{
-    park(){
-        return 'stopped';
-    }
+beforeEach(async() => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+    
+    // Use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['first deploy']})
+        .send({from: accounts[0], gas: '1000000'});
+})
 
-    drive(){
-        return 'vroom';
-    }
-}
+describe('Inbox',()=>{
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
+    });
 
-
-describe('Car calss',() => {
-    it('can park',()=>{
-        const car = new Car();
-        assert.equal(car.park(), 'stopped');
-    })
-
-    it('can drive', () => {
-        const car = new Car();
-        assert.equal(car.drive(),'vroom');
+    it('has a default message',async ()=>{
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'first deploy')
     })
 })
