7a7ba69610dfd7af9d287ae79b803d031d0f9581
Steven <stntsai93@gmail.com>
2021-12-02 14:45:55 -0500

    test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,29 @@
 // contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+
+class Car{
+    park(){
+        return 'stopped';
+    }
+
+    drive(){
+        return 'vroom';
+    }
+}
+
+
+describe('Car calss',() => {
+    it('can park',()=>{
+        const car = new Car();
+        assert.equal(car.park(), 'stopped');
+    })
+
+    it('can drive', () => {
+        const car = new Car();
+        assert.equal(car.drive(),'vroom');
+    })
+})
