1044d38f7d3b383919221e4cf5a2a8e21ae16c3e
Steven <stntsai93@gmail.com>
2021-12-02 14:16:15 -0500

    compile

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,4 +1,16 @@
 pragma solidity ^0.4.17;
 // linter warnings (red underline) about pragma version can igonored!
 
+contract Inbox{
+    string public message;
+
+    function Inbox(string initialMessage) public{
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public{
+        message = newMessage;
+    }
+
+}
 // contract code will go here
