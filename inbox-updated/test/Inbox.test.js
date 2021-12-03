457d581e3b154c999b010e7f78e681464520e728
Steven <stntsai93@gmail.com>
2021-12-02 19:05:33 -0500

    update version

---
+++ inbox-updated/test/Inbox.test.js
@@ -0,0 +1,36 @@
+// contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode} = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async() => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+    
+    // Use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['first deploy']})
+        .send({from: accounts[0], gas: '1000000'});
+})
+
+describe('Inbox',()=>{
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message',async ()=>{
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'first deploy')
+    })
+
+    it('can set new message', async () => {
+        await inbox.methods.setMessage('new message').send({from: accounts[0]});
+        const newMessage = await inbox.methods.message().call();
+        assert.equal(newMessage, 'new message')
+    })
+})
